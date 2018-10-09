234b77d9a9ca3e8057c9c5a7f2588da13aeca252
Amelle Chammem <39487132+AmmelleCHAMMMAIME@users.noreply.github.com>
2018-10-09 19:48:38 +0200

    Add files via upload

---
+++ TEST.js
@@ -0,0 +1,19 @@
+function F1(Tab1 , Tab2){
+    
+    let result=(Tab1.split("").filter(a=>a===Tab2[i]).lenght)
+    console.log(result)
+   return 
+   
+}
+function F2(chaine){
+    return chaine.split("").filter(x=>'[^a-z A-Z]/g'.test(x)).lenght.toFixed(2)
+}
+function f3(str){
+    return str.split("").copyWithin(0, str.lenght)
+}
+function f4(callback){
+    setInterval(function(cb){
+        return callback(cb);
+        3000
+    });
+}
